726b539516823a6595baa84de3884e96f10caf96
guan yuan <yuanguan89@gmail.com>
2014-07-16 15:30:30 +0800

    use spring mvc to operate addtion

--- web/src/main/java/com/thoughtworks/hello/HelloWorldController.java
+++ web/src/main/java/com/thoughtworks/hello/HelloWorldController.java
@@ -1,15 +1,31 @@
 package com.thoughtworks.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class HelloWorldController{
+    @Autowired
+    private NumberCalculator numberCalculator;
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String printHello(ModelMap model) {
+        return "display";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String add(ModelMap model, HttpServletRequest request, HttpServletResponse response) {
+        String adder1 = request.getParameter("adder1");
+        String adder2 = request.getParameter("adder2");
+        String result = numberCalculator.add(adder1, adder2);
+        model.addAttribute("addResult", result);
         return "display";
     }
 }
--- web/src/main/webapp/WEB-INF/pages/display.jsp
+++ web/src/main/webapp/WEB-INF/pages/display.jsp
@@ -1,7 +1,7 @@
 <html>
 <body>
 
-<form action="hello" method="post">
+<form action="add" method="post">
   <p>Adder1: <input type="text" name="adder1"/></p>
   <p>Adder2: <input type="text" name="adder2"/></p>
   <input type="submit" value="Submit" />
@@ -10,9 +10,10 @@
 
 <%
 String result = (String)request.getAttribute("addResult");
-if(result != null)
-    out.println(result);
-%>
+if(result != null){%>
+<h2>The result is:
+    <%out.println(result);}%>
+<h2>
 
 
 </body>
