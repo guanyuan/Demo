0ac2e257b1119f89ac7ab967c21a0e1cee3734ab
guan yuan <yuanguan89@gmail.com>
2014-07-17 11:02:54 +0800

    delete files related with Calculator

--- core/src/main/java/com/thoughtworks/hello/Application.java
+++
@@ -1,7 +0,0 @@
-package com.thoughtworks.hello;
-
-public class Application {
-    public static void main(String[] args) {
-
-    }
-}
--- core/src/main/java/com/thoughtworks/hello/ICalculator.java
+++
@@ -1,8 +0,0 @@
-package com.thoughtworks.hello;
-
-/**
- * Created by yguan on 7/14/14.
- */
-public interface ICalculator {
-    String add(String adder1, String adder2);
-}
